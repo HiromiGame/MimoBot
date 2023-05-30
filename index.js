ac4ee4b7e97457e3111211737e117005ec77eed9
HiromiGame <csidemt.game@gmail.com>
2023-05-30 15:06:47 +0900

    更新

--- index.js
+++ index.js
@@ -18,17 +18,27 @@ const commandFoldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(commandFoldersPath);
 
 for (const folder of commandFolders) {
+
     const commandsPath = path.join(commandFoldersPath, folder);
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
     for (const file of commandFiles) {
+
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
+
         if ('data' in command && 'execute' in command) {
+
             client.commands.set(command.data.name, command);
+
         } else {
+
             console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+
         };
+
     };
+
 };
 
 
@@ -37,17 +47,27 @@ const eventFoldersPath = path.join(__dirname, 'events');
 const eventFolders = fs.readdirSync(eventFoldersPath);
 
 for (const folder of eventFolders) {
+
     const eventsPath = path.join(eventFoldersPath, folder);
     const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
     for (const file of eventFiles) {
+
         const filePath = path.join(eventsPath, file);
         const event = require(filePath);
+
         if (event.once) {
+
             client.once(event.name, (...args) => event.execute(...args));
+
         } else {
+
             client.on(event.name, (...args) => event.execute(...args));
+
         };
+
     };
+
 };
 
 
